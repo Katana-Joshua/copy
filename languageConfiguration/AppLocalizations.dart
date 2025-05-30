baf11af853445558aafafe619f47d0d551336257
Katana-Joshua <60971033+Katana-Joshua@users.noreply.github.com>
2025-05-30 07:19:14 +0300

    Add files via upload

---
+++ languageConfiguration/AppLocalizations.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'BaseLanguage.dart';
+import 'LanguageDataConstant.dart';
+
+class AppLocalizations extends LocalizationsDelegate<BaseLanguage> {
+  const AppLocalizations();
+
+  @override
+  Future<BaseLanguage> load(Locale locale) async {
+    switch (locale.languageCode) {
+      case 'en':
+        return BaseLanguage();
+      default:
+        return BaseLanguage();
+    }
+  }
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'en-IN'].contains(locale.languageCode);
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<BaseLanguage> old) => false;
+}
